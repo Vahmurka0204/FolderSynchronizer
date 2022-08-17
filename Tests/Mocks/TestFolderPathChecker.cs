0946198c2338b163ff69af8d52cbe8b677db79da
Alexandra <sasha-021248@yandex.ru>
2022-08-17 15:37:54 +0200

    clean up Tests

--- Tests/Mocks/TestFolderPathChecker.cs
+++ Tests/Mocks/TestFolderPathChecker.cs
@@ -1,7 +1,7 @@
-﻿using System.IO;
-using System.Text.RegularExpressions;
+﻿using System.Text.RegularExpressions;
+using FolderSynchronizerLib;
 
-namespace FolderSynchronizerLib
+namespace Tests
 {
     public class TestFolderPathChecker : IChecker
     {
