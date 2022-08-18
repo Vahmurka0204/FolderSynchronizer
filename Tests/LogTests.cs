b10388f469708918ea386b809e7b92c47fcfc65b
Alexandra <sasha-021248@yandex.ru>
2022-08-18 13:52:34 +0200

    add log tests

---
+++ Tests/LogTests.cs
@@ -0,0 +1,23 @@
+﻿using Xunit;
+using FolderSynchronizerLib;
+
+namespace Tests
+{
+    public class LogTests
+    {
+        [Fact]
+        public void VerboseLog()
+        {
+            var logCreator = new LogCreator();
+            var log = logCreator.Create(LogLevels.Summary);
+            for (int i = 0;i<5;i++)
+                log.GetInfoAboutAddFiles("","");
+            for (int i = 0; i < 3; i++)
+                log.GetInfoAboutUpdateFiles("", "");
+            for (int i = 0; i < 7; i++)
+                log.GetInfoAboutDeleteFiles("");
+
+            Assert.Equal("5 files have been added\n3 files have been updated\n7 files have been deleted", log.FormLogToPrint());
+        }
+    }
+}
